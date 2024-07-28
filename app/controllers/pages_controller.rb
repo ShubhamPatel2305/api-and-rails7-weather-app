7d1788ff980d9bccc212728b7e50d97cf5d3e630
ShubhamPatel2305 <shubhamcp23@gmail.com>
2024-07-28 20:34:41 +0530

    how to access data inside simple and array

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,11 @@
+require 'uri'
+require 'net/http'
+
+
 class PagesController < ApplicationController
   def home
+    uri = URI('https://api.openweathermap.org/data/2.5/weather?lat=23.0225&lon=72.5714&appid=466d7abbe009466948b7a6019a822fe5&units=metric')
+    res = Net::HTTP.get_response(uri)
+    @data = JSON.parse(res.body)
   end
 end
